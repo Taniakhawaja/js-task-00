8b9938d6b5dd4a4062e13ca44929aa2da3496c55
Tania-khawaja <145832884+Taniakhawaja@users.noreply.github.com>
2024-02-17 12:56:43 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// exercise 1 //
+
+ /*let myNum = 493;
+let sum = 0;
+while(myNum){
+    sum += myNum % 10;
+    myNum = (myNum / 10) | 0;
+}
+    console.log(sum);*/
+
+    // exercise 2 //
+/*const Num1 = [1,2,3];
+const Num2 = [4,5,6];
+let sum = 0;
+for(let i = 0 ; i < Num1.length ; i++ ){
+    sum += Num1[i] + Num2[i];
+}
+console.log(sum)*/
+
+// exercise 3 //
+
+/*first calculate the sum of the numbers in the arrays using a for loop then 
+devides it by the num of elements to find average*/
+
+/*const numbers = [2,3,4,5];
+let sum = 0;
+for(let i = 0;  i < numbers.length; i ++) {
+        sum += numbers[i];
+    }
+    const average = sum / numbers.length;
+    console.log(average);*/
+
+     // exercise 4 //
+/*it counts the characters of strings and  print them out through console */
+
+     /*const string1 = "car";
+     const string2 = "care";
+     if
+       (string1.length === string2.length) {
+        console.log("strings are equal");
+     }
+     else {
+        console.log("strings are not equal");
+    }*/
+
+    // exercise 5 //
+
+    
